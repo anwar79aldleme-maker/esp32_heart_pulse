9618a0da5e65be06a9674396a48fbb51ac093cab
anwar79aldleme-maker <anwar79aldleme@gmail.com>
2026-01-23 14:06:35 +0300

    Add files via upload

---
+++ api/latest.js
@@ -0,0 +1,24 @@
+import { Pool } from "@neondatabase/serverless";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default async function handler(req, res) {
+  try {
+    const result = await pool.query(
+      `SELECT signal, created_at
+       FROM sensor_data
+       ORDER BY created_at ASC`
+    );
+
+    res.status(200).json(result.rows);
+
+  } catch (error) {
+    console.error("LATEST API ERROR:", error);
+    res.status(500).json({
+      error: "Database error",
+      details: error.message
+    });
+  }
+}

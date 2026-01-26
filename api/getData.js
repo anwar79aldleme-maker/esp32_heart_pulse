c526878c3fb2e49a57f867988eea84bece63b717
anwar79aldleme-maker <anwar79aldleme@gmail.com>
2026-01-26 10:44:13 +0300

    Update getData.js

--- api/getData.js
+++ api/getData.js
@@ -1,18 +1,20 @@
-
-import { sql } from './db';
+import { sql } from "./db";
 
 export default async function handler(req, res) {
   const { device_id } = req.query;
+
   try {
     const data = await sql`
-      SELECT signal, bpm FROM pulse_data
+      SELECT signal, bpm 
+      FROM pulse_data
       WHERE device_id = ${device_id}
       ORDER BY id DESC
       LIMIT 300
     `;
+
     res.status(200).json(data.reverse());
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error.message);
     res.status(500).json({ error: "Server error" });
   }
 }
